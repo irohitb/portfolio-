6da6b956809e3ee2f9314bf4b29e431881181f22
irohitb <irohitbhatial@gmail.com>
2018-08-23 23:32:50 +0530

    UI Update

--- src/UI/ServiceBoxes/serviceBoxes.js
+++ src/UI/ServiceBoxes/serviceBoxes.js
@@ -7,7 +7,7 @@ export const ServiceBoxesLeft = (props) => {
     let image = images("./" + props.image)
 
 return (
-        <div className="service-left-main"> 
+        <div className="container service-left-main"> 
          
                 <div className="row"> 
                     <div className="col-12 col-sm-12 col-md-6 col-lg-6">
@@ -33,8 +33,9 @@ export const ServiceBoxesRight = (props) => {
     let image = images("./" + props.image)
 
     return (
+        <div className="container">
+        <div className="d-none d-md-block">
             <div className="service-right-main"> 
-           
                     <div className="row"> 
                         <div className="col-12 col-sm-12 col-md-6 col-lg-6">
                             <div className="text-class"> 
@@ -48,8 +49,26 @@ export const ServiceBoxesRight = (props) => {
                             </div>   
                         </div>
                     </div>
-   
             </div>       
+        </div>
+        <div className="d-block d-md-none">
+            <div className="service-left-main"> 
+                <div className="row"> 
+                    <div className="col-12 col-sm-12 col-md-6 col-lg-6">
+                        <div className="image-class"> 
+                        <img src={image} className="service-images"/> 
+                        </div>
+                    </div>
+                    <div className="col-12 col-sm-12 col-md-6 col-lg-6">
+                        <div className="text-class"> 
+                        <h2 className="text-heading"> {props.heading} </h2>
+                        <p className="para-text"> {props.text}</p>
+                        </div>
+                    </div>
+               </div>
+        </div>     
+    </div>
+</div>
             ) 
     }
     
--- src/container/home/content.js
+++ src/container/home/content.js
@@ -36,15 +36,15 @@ class content extends Component  {
                     <div className="container"> 
                         <h4 className="content-service-heading content-1"> What we do? </h4>
                         <h2 className="content-service-heading content-2">We make community of happy customers and strong bonds with our buisness partners by providing world class services</h2>
-
+                        <div className="borderline"> </div>
                         <div className="row">
                             <div className="col-md-4 col-lg-4"> 
                                 <div className="main-sub-box">
                                     <div className="row">
-                                        <div className="col-md-4 col-lg-4 boxes-image">
+                                        <div className="col-md-12 boxes-image">
                                             <img src={Printing} className="boxes-image-image" />
                                         </div>
-                                    <h4 className="col-md-7 col-lg-7 content-heading-b" value="printing"> Printing Solutions </h4>
+                                    <h4 className="content-heading-b" value="printing"> Printing Solutions </h4>
                                         <p class="content-para-b"> With our advance Hardware, Software and Team of experts, we provide the best printing solution to our customers and business Partners at an unbeatable low price
                                         </p>
                                         <button className="content-para-b class-green" name="printing" onClick={this.contentTodisplay}>Click here to check out our services</button>
@@ -54,10 +54,10 @@ class content extends Component  {
                             <div className="col-md-4 col-lg-4"> 
                                 <div className="main-sub-box">
                                     <div className="row">
-                                        <div className="col-md-4 col-lg-4 boxes-image">
+                                        <div className="col-md-12 boxes-image">
                                             <img src={Carwash} className="boxes-image-image" />
                                         </div>
-                                    <h4 className="col-md-7 col-lg-7 content-heading-b"> Quality Car Wash </h4>
+                                    <h4 className=" content-heading-b"> Quality Car Wash </h4>
                                         <p class="content-para-b"> From using the latest technology to providing door to door car wash services, we are commited to make your car shine the brightest in the area at an exceptionally affordable price.
                                         </p>
                                         <button className="content-para-b class-green" name="carwash" onClick={this.contentTodisplay}>Click here to check all our services</button>
@@ -67,10 +67,10 @@ class content extends Component  {
                             <div className="col-md-4 col-lg-4"> 
                                 <div className="main-sub-box">
                                     <div className="row">
-                                        <div className="col-md-4 col-lg-4 boxes-image">
+                                        <div className="col-md-12 boxes-image">
                                             <img src={Manpower} className="boxes-image-image" />
                                         </div>
-                                    <h4 className="col-md-7 col-lg-7 content-heading-b" value="man-power"> Man-Power </h4>
+                                        <h4 className=" content-heading-b"> Man Power Supply </h4>
                                         <p class="content-para-b"> We supply our Manpower on hourly basis for various roles in Industries as per their requirements. We give you liberity to chose from highly skilled to semi-skilled to unskilled. 
                                         </p>
                                         <button className="content-para-b class-green" name="manpower" onClick={this.contentTodisplay}>Click here to check all our services</button>
